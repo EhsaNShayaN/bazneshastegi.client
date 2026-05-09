d9aa9a0266d51c0126c95d161b6d4a1622b63fc9
EhsaNShayaN <EhsaN.HaresAbady@gmail.com>
2026-05-09 09:46:39 +0330

    change ports

--- src/proxy.conf.js
+++ src/proxy.conf.js
@@ -1,7 +1,7 @@
 const {env} = require('process');
 
 const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7232';
+  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7233';
 
 const PROXY_CONFIG = [
   {
