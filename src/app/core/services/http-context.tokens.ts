b1a6a71ba9d82d8e4dfa12a1940754f87baa0040
EhsaNShayaN <EhsaN.HaresAbady@gmail.com>
2026-07-30 13:04:58 +0330

    -cookie -captcha

---
+++ src/app/core/services/http-context.tokens.ts
@@ -0,0 +1,46 @@
+import {Injectable} from '@angular/core';
+import {HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpResponse} from '@angular/common/http';
+import {Observable, tap} from 'rxjs';
+import {AuthService} from './auth.service';
+import {ToastrService} from 'ngx-toastr';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthInterceptor implements HttpInterceptor {
+  constructor(private auth: AuthService,
+              private toaster: ToastrService) {
+  }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const token = this.auth.getToken();
+    let cloned: HttpRequest<any> = req;
+    if (token) {
+      cloned = req.clone({
+        setHeaders: {Authorization: `Bearer ${token}`}
+      });
+    }
+    return next.handle(cloned).pipe(
+      tap({
+        next: (event) => {
+          if (event instanceof HttpResponse) {
+            const responseBody = event.body;
+            if (responseBody?.isSuccess) {
+            } else {
+              if (responseBody.errors?.length > 0) {
+                for (const error of responseBody.errors) {
+                  console.error('error', error);
+                  //console.log(error.errorCode, error.errorMessage);
+                  this.toaster.error(error.errorMessage, error.errorCode, {});
+                }
+              }
+            }
+          }
+        },
+        error: (error: HttpErrorResponse) => {
+          //console.error('❌ API Error:', cloned.url, error.message);
+        }
+      })
+    );
+  }
+}
