3d0814df713fbe6bdb40ed6f6cb472cb1e889296
EhsaNShayaN <EhsaN.HaresAbady@gmail.com>
2026-08-15 17:23:15 +0330

    .

---
+++ src/app/core/models/CalculateMedicalTreatmentCostResponse.ts
@@ -0,0 +1,27 @@
+import {BaseResult} from './BaseResult';
+
+export interface GetRequestTypeConfigResponse extends BaseResult<RequestTypeConfigInfo[]> {
+}
+
+export interface RequestTypeConfigInfo {
+  requestTypeConfigID: string | null;
+  requestTypeID: string;
+  lookupID: number;
+  defaultAmount: number;
+  deliveryCost?: number;
+  defaultInstalementCount: number | null;
+  defaultDiscountPercent: number | null;
+  profitOrDiscountPercent: number | null;
+  guarantorCost: number | null;
+  validationStartDate: string;
+  facilityReceiverRelationshipID: string | null;
+  pensionaryStatusCategory: string | null;
+  genderLookupID: number | null;
+  isActive: boolean;
+  requestTypeName: string;
+  lookupName: string;
+  facilityReceiverRelationshipName: string | null;
+  pensionaryStatusCategoryName: string | null;
+  genderName: string | null;
+  defaultInstalementAmount: number | null;
+}
